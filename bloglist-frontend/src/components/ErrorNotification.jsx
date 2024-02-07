4fcf2dbb8550e8ee2e4cda704f6400c5f7baae46
Kien Nguyen <nsongkien@gmail.com>
2024-02-07 21:53:19 +0200

    Delete bloglist-frontend directory

--- bloglist-frontend/src/components/ErrorNotification.jsx
+++
@@ -1,9 +0,0 @@
-const ErrorNotification = ({ errorMessage }) => {
-  if(errorMessage===null)
-    return null
-  return <div className="error">
-    {errorMessage}
-  </div>
-}
-
-export default ErrorNotification
